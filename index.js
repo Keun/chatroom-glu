f47d336785d0961d120ad0c299de8262e79d1123
Koen <kborghols@glu.nl>
2020-05-15 14:56:16 +0200

    test babel-node

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
